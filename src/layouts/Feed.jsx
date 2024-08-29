2bcd3d4bfde9215d48e765255da926e4794d9247
berkayyyilmaz <yilmaz.berkayyagiz@gmail.com>
2024-08-29 12:02:19 +0300

    tweetbox test

--- src/layouts/Feed.jsx
+++ src/layouts/Feed.jsx
@@ -1,7 +1,24 @@
+import designsta from '../assets/designsta.png'
 
 export default function Feed() {
   return (
-        <main className="flex-1 bg-red-400">Feed</main>
+        <main className="flex-1 border">
+          <div className="tweet-container flex min-h-24">
+            <div className="pp-container min-w-10 p-2">
+              <img src={designsta} alt="Profile Picture" className='w-12' />
+            </div>
+            <div className="tw-details">
+              <div className="tw-owner ">
+                <div className='flex'>
+                <span className='font-bold mr-2'>Designsta</span>
+                <h1>@inner * 25m</h1>
+                </div>
+                <h1 className="tweet">Tweet Bu</h1>
+              </div>
+              <div className="tw-stats"></div>
+            </div> 
+          </div>
+        </main>
         
 
   )
